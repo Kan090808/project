e7719b8a427acbd48ebcba22aa0345c03723f6d2
Unknown <junanyeap@gmail.com>
2018-10-10 21:01:23 +0800

    bug fix

--- html/model.php
+++ html/model.php
@@ -198,7 +198,7 @@ function getClient($type)
     $client->setAccessToken($_SESSION['access_token']);
   } else {
     if ($_SERVER['HTTP_HOST'] == "163.22.17.92") {
-      $redirect_uri = 'http://' . $_SERVER['HTTP_HOST'] . '.nip.io/oauth2callback.php';
+      $redirect_uri = 'http://' . $_SERVER['HTTP_HOST'] . '.nip.io/project/html/oauth2callback.php';
     } else {
       $redirect_uri = 'http://' . $_SERVER['HTTP_HOST'] . '/project/html/oauth2callback.php';
     }
