bc57d3a9db5c60991e9fcde49d0e66f101108295
ngyankee092439 <s104213059@mail1.ncnu.edu.tw>
2018-10-10 19:26:38 +0800

    Jquery changed to jquery

--- website/sidebar.php
+++ website/sidebar.php
@@ -2,9 +2,9 @@
 <aside class="main-sidebar hidden-print ">
   <section class="sidebar" id="sidebar-scroll">
 
-    <div class="user-panel col-sm-12">
-      <div class="f-left image col-sm-4"><img src="assets/images/avatar-1.png" alt="User Image" class="img-circle"></div>
-      <div class="f-left info col-sm-8">
+    <div class="user-panel">
+      <div class="f-left image"><img src="assets/images/avatar-1.png" alt="User Image" class="img-circle"></div>
+      <div class="f-left info">
         <?php if ($name == "未登入") {
           echo '<p class="col-sm-6">' . $name . '</p><form class="col-sm-6" action="../html/control.php" method="post"><button class="btn btn-sm" type="submit" value="getClient" name="act">登入</button></form> ';
         } else {
